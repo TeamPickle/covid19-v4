a7e73a49986dd5233d5fce0fff8cda8e1d4828e2
cra1nbow <dev@cra1nbow.is>
2022-08-18 10:51:33 +0900

    feat: world stat basic

--- function/commands/world/command.go
+++ function/commands/world/command.go
@@ -2,39 +2,46 @@ package world
 
 import (
 	"context"
+	"fmt"
 	"function/external/coronaboard"
 	"function/utils"
 	"math"
-	"register/config"
 
 	"github.com/diamondburned/arikawa/v3/api"
 	"github.com/diamondburned/arikawa/v3/discord"
 )
 
 func (*WorldCommand) Handle(ctx context.Context, data *discord.CommandInteraction, rawRequest discord.InteractionEvent) *api.InteractionResponse {
-	go func() {
-		coronaboardData, err := coronaboard.ParseBoard(ctx)
-		if err != nil {
-			resp := utils.MakeErrorMessage(rawRequest, "Can't get coronaboard data")
-			client.EditInteractionResponse(config.AppID, rawRequest.Token, api.EditInteractionResponseData{Embeds: resp.Data.Embeds})
-			return
-		}
-		client.EditInteractionResponse(config.AppID, rawRequest.Token, api.EditInteractionResponseData{
-			Embeds: &[]discord.Embed{makeMainEmbed(&mainData{
-				active:         coronaboardData.ChartForGlobal.Global.Active[len(coronaboardData.ChartForGlobal.Global.Active)-1],
-				confirmed:      coronaboardData.ChartForGlobal.Global.ConfirmedAcc[len(coronaboardData.ChartForGlobal.Global.ConfirmedAcc)-1],
-				confirmedDelta: coronaboardData.ChartForGlobal.Global.Confirmed[len(coronaboardData.ChartForGlobal.Global.Confirmed)-1],
-				released:       coronaboardData.ChartForGlobal.Global.ReleasedAcc[len(coronaboardData.ChartForGlobal.Global.ReleasedAcc)-1],
-				releasedDelta:  coronaboardData.ChartForGlobal.Global.Released[len(coronaboardData.ChartForGlobal.Global.Released)-1],
-				death:          coronaboardData.ChartForGlobal.Global.DeathAcc[len(coronaboardData.ChartForGlobal.Global.DeathAcc)-1],
-				deathDelta:     coronaboardData.ChartForGlobal.Global.Death[len(coronaboardData.ChartForGlobal.Global.Death)-1],
-				countries:      int64(len(coronaboardData.StatGlobalNow)),
-				totalPages:     int(math.Ceil(float64(len(coronaboardData.StatGlobalNow))/10)) + 1,
-			})},
-		})
-	}()
-
-	return &api.InteractionResponse{
-		Type: api.DeferredMessageInteractionWithSource,
+	coronaboardData, err := coronaboard.ParseBoard(ctx)
+	if err != nil {
+		panic(err)
 	}
+	embedData := mainData{
+		active:         coronaboardData.ChartForGlobal.Global.Active[len(coronaboardData.ChartForGlobal.Global.Active)-1],
+		confirmed:      coronaboardData.ChartForGlobal.Global.ConfirmedAcc[len(coronaboardData.ChartForGlobal.Global.ConfirmedAcc)-1],
+		confirmedDelta: coronaboardData.ChartForGlobal.Global.Confirmed[len(coronaboardData.ChartForGlobal.Global.Confirmed)-1],
+		released:       coronaboardData.ChartForGlobal.Global.ReleasedAcc[len(coronaboardData.ChartForGlobal.Global.ReleasedAcc)-1],
+		releasedDelta:  coronaboardData.ChartForGlobal.Global.Released[len(coronaboardData.ChartForGlobal.Global.Released)-1],
+		death:          coronaboardData.ChartForGlobal.Global.DeathAcc[len(coronaboardData.ChartForGlobal.Global.DeathAcc)-1],
+		deathDelta:     coronaboardData.ChartForGlobal.Global.Death[len(coronaboardData.ChartForGlobal.Global.Death)-1],
+		countries:      int64(len(coronaboardData.StatGlobalNow)),
+		totalPages:     int(math.Ceil(float64(len(coronaboardData.StatGlobalNow))/10)) + 1,
+	}
+	return utils.MessageInteractionResponseWithSource(&api.InteractionResponseData{
+		Embeds: &[]discord.Embed{makeMainEmbed(&embedData)},
+		Components: discord.ComponentsPtr(
+			&discord.ActionRowComponent{
+				&discord.ButtonComponent{
+					Style:    discord.PrimaryButtonStyle(),
+					CustomID: discord.ComponentID(fmt.Sprintf("%s-%d", rawRequest.SenderID(), embedData.totalPages-1)),
+					Emoji:    &discord.ComponentEmoji{Name: "◀️"},
+				},
+				&discord.ButtonComponent{
+					Style:    discord.PrimaryButtonStyle(),
+					CustomID: discord.ComponentID(fmt.Sprintf("%s-%d", rawRequest.SenderID(), 1)),
+					Emoji:    &discord.ComponentEmoji{Name: "▶️"},
+				},
+			},
+		),
+	})
 }
