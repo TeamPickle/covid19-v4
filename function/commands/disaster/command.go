8edbd4eab075d9250e103d46caa61880727a819e
cra1nbow <dev@cra1nbow.is>
2022-08-18 15:58:08 +0900

    refactor: remove unused comments

--- function/commands/disaster/command.go
+++ function/commands/disaster/command.go
@@ -18,63 +18,6 @@ var regions = []string{
 	"전국", "강원", "경기", "경남", "경북", "광주", "대구", "대전", "부산", "서울", "울산", "인천", "전남", "전북", "제주", "충남", "충북", "세종",
 }
 
-/*
-const location = await getLocation(msg.author.id, args[1]);
-    if (!location) {
-      return msg.respond(stripIndents`
-        명령어 사용법 : \`${getGuildPrefix(msg.guild)}재난문자 [지역]\`
-        지역 목록 : \`${disasterData.disasterRegion.join(' ')}\`
-      `);
-    }
-    const u = (
-      Object.keys(disasterData.disasterAlias).includes(location)
-        ? disasterData.disasterAlias[
-            location as keyof typeof disasterData.disasterAlias
-          ]
-        : location
-    ) as typeof disasterData.disasterRegion[number];
-
-    const disasterIndex = disasterData.disasterRegion.indexOf(u);
-
-    if (disasterIndex < 0) {
-      return msg.respond(stripIndents`
-        지원하지 않는 지역입니다. 다음 지역 중 하나로 다시 시도해주세요.
-        \`${disasterData.disasterRegion.join(' ')}\`
-      `);
-    }
-
-    const source: string = (
-      await (
-        await fetch(
-          `https://m.search.naver.com/p/csearch/content/nqapirender.nhn?where=m&pkid=258&key=disasterAlert&u1=${
-            disasterIndex ? disasterIndex.toString().padStart(2, '0') : ''
-          }`,
-        )
-      ).json()
-    ).current.html;
-
-    const local = [...source.matchAll(/<em class="area_name">(.+?)<\/em>/g)];
-    const con = [...source.matchAll(/<span class="dsc _text">(.+?)<\/span>/g)];
-    const distime = [...source.matchAll(/<time datetime="">(.+?)<\/time>/g)];
-
-    if (local.length === 0 && con.length === 0) {
-      return msg.respond('재난문자를 불러올 수 없습니다.');
-    }
-
-    const embed = new MessageEmbed();
-    embed
-      .setTitle('📌 재난문자')
-      .setDescription(`${u} 지역의 최근 5개 재난문자 목록입니다.`)
-      .setColor(0xdd2255)
-      .addFields(
-        [...Array(5)].map((_, i) => ({
-          name: `${local[i][1]}(${distime[i][1]})`.slice(0, 256),
-          value: con[i][1],
-        })),
-      );
-    return msg.respond({ embeds: [embed] });
-*/
-
 func (*DisasterCommand) Handle(ctx context.Context, data *discord.CommandInteraction, rawRequest discord.InteractionEvent) *api.InteractionResponse {
 	region := data.Options.Find("province").String()
 	regionIndex := -1
